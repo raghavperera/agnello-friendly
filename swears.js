034833f3a5165f81593c5a3f63d89d28aad458e7
raghavperera <pereraraghav@gmail.com>
2025-08-14 23:55:46 -0400

    Update swears.js

--- swears.js
+++ swears.js
@@ -1,1 +1,42 @@
+// swears.js - list of banned words for Agnello FC bot
+// Add or remove words as needed
+
+module.exports = [
+  'badword1',
+  'badword2',
+  'badword3',
+  'badword4',
+  'badword5',
+  'badword6',
+  'badword7',
+  'badword8',
+  'badword9',
+  'badword10',
+  'curseword1',
+  'curseword2',
+  'curseword3',
+  'curseword4',
+  'curseword5',
+  'curseword6',
+  'curseword7',
+  'curseword8',
+  'curseword9',
+  'curseword10',
+  // add more to make it comprehensive
+  'damn',
+  'hell',
+  'shit',
+  'fuck',
+  'bitch',
+  'asshole',
+  'dick',
+  'piss',
+  'crap',
+  'faggot',
+  'nigger',
+  'cunt',
+  'whore',
+  'slut',
+  'bastard'
+];
 
